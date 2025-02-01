43a2376dd7fad0beed0b73dd987ace74baf38291
Andrew Molchanov <mr.catmanrus@gmail.com>
2025-02-02 01:37:59 +0300

    Обновил и перенёс примерный модуль

---
+++ packages/example-module/src/module.ts
@@ -0,0 +1,21 @@
+export class Module {
+  static readonly #name = "ExampleModule";
+  static readonly #version = "1.0.0";
+  static readonly #description = "Example module";
+  static readonly #author = "Example author";
+
+  static getInfo() {
+    return {
+      name: this.#name,
+      version: this.#version,
+      description: this.#description,
+      author: this.#author,
+    };
+  }
+
+  // TODO Add app type
+  init(app: any) {
+    console.log("Example module initialized");
+    console.log(app);
+  }
+}
